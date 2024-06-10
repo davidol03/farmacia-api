c830499b1fdeee0ea5ea86a56b2f76da8e508782
davidol03 <165960045+davidol03@users.noreply.github.com>
2024-06-10 15:26:04 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import {DB_HOST,DB_DATABASE,DB_PORT,URL} from './config.js'
-import rutasMedicamentos from './Routes/Medicamentos.routes.js'
-import rutasAuth from './Routes/Auth.routes.js'
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import {DB_HOST,DB_DATABASE,DB_PORT,URL} from './config.js';
+import rutasMedicamentos from './Routes/Medicamentos.routes.js';
+import rutasAuth from './Routes/Auth.routes.js';
 //const conexion = 'mongodb://'+DB_HOST+':'+DB_PORT+'/'+DB_DATABASE
-mongoose.connect(URL).then()
+mongoose.connect(URL).then();
 
-const app = express()
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.static('public'))
-app.use(rutasMedicamentos)
-app.use(rutasAuth)
+const app = express();
+const corsOptions = {
+    origin: ['http://localhost:5173','https://'],
+    credentials: true
+};
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(rutasMedicamentos);
+app.use(rutasAuth);
 
 app.use((req, res) => {
-    res.status(404).json({status:false,errors:'Not found'})
-} )
+    res.status(404).json({status:false,errors:'Not found'});
+});
 //app.get('/', (req,res) => { res.send('Hola mundo')})
-export default app
+export default app;
